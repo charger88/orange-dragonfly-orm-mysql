7f58b88937e27a432ed4263bbdd04b29847da30f
Michael Kelner <mikhail.kelner@gmail.com>
2020-02-06 21:46:19 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Orange Dragonfly ORM MySQL Driver
+
+One day Orange Dragonfly will become a NodeJS framework. For now I'm starting to publish its components.
+
+This library is MySQL Driver for Orange Dragonfly ORM
---
+++ index.js
@@ -0,0 +1,34 @@
+const ORM = require('orange-dragonfly-orm');
+const mysql = require('mysql');
+
+class MySQLDriver extends ORM.AbstractDB {
+
+  connect (){
+    return new Promise((resolve, reject) => {
+      this.connection = mysql.createConnection({
+        host: this.config.host,
+        port: this.config.port || 3306,
+        user: this.config.user,
+        password: this.config.password,
+        database: this.config.database,
+        connectTimeout: this.config.connect_timeout || 5000
+      });
+      this.connection.connect((err) => {
+        err ? reject(err) : resolve(this.connection);
+      });
+    })
+  }
+
+  disconnect () {
+    if (this.connection) {
+      try {
+        this.connection.end()
+      } catch (e) {} finally {
+        this.connection = null;
+      }
+    }
+  }
+
+}
+
+module.exports = MySQLDriver
